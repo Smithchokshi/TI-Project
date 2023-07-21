f58d1a1a5059743f0a96596f186ba6f852db94fe
Smith Chokshi <sm714486@dal.ca>
2023-07-21 00:20:21 -0300

    implemented authentication feature

--- termprojectteam8/src/App.js
+++ termprojectteam8/src/App.js
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './routing';
+import './App.css';
 
 const App = () => {
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   return (
-        <BrowserRouter>
-          <Routing />
-        </BrowserRouter>
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
   );
 };
 
--- termprojectteam8/src/routing.js
+++ termprojectteam8/src/routing.js
@@ -5,62 +5,61 @@ import { Layout } from 'antd';
 const Login = lazy(() => import('./Component/Login/login'));
 const Register = lazy(() => import('./Component/Register/register'));
 
-const { Content } = Layout;
-
 const Routing = () => {
-    const isAuthenticated = localStorage.getItem('isAuthenticated');
-    const navigate = useNavigate();
-    const PublicRoutes = [
-        {
-            path: '/',
-            component: <Login />,
-        },
-        {
-            path: '/login',
-            component: <Login />,
-        },
-        {
-            path: '/register',
-            component: <Register />,
-        },
-    ].filter(cur => cur);
+  const isAuthenticated = localStorage.getItem('isAuthenticated');
+  console.log(isAuthenticated);
+  const navigate = useNavigate();
+  const PublicRoutes = [
+    {
+      path: '/',
+      component: <Login />,
+    },
+    {
+      path: '/login',
+      component: <Login />,
+    },
+    {
+      path: '/register',
+      component: <Register />,
+    },
+  ].filter(cur => cur);
 
-    const PrivateRoutes = [].filter(cur => cur);
+  const PrivateRoutes = [].filter(cur => cur);
 
-    const PrivateRoute = ({ children }) => {
-        if(!isAuthenticated) navigate('/login', {replace: true})
-        return isAuthenticated ? children : <Login />;
-    };
+  const PrivateRoute = ({ children }) => {
+    if (!isAuthenticated) navigate('/login', { replace: true });
+    return isAuthenticated === 'false' ? children : <Login />;
+  };
 
-    const PublicRoute = ({ children }) => {
-        // if(isAuthenticated) navigate('/products', {replace: true})
-        return isAuthenticated ? <></> : children;
-    };
+  const PublicRoute = ({ children }) => {
+    // if(isAuthenticated) navigate('/products', {replace: true})
+    return isAuthenticated === 'true' ? <>Private</> : children;
+  };
 
-    return (
-        <Suspense className="loader" fallback={<></>}>
-            <Layout style={{ minHeight: '100vh', display: 'flex' }}>
-                <Routes>
-                    {PublicRoutes.map(route => (
-                        <Route
-                            exact={route.exact}
-                            key={route.path}
-                            path={route.path}
-                            element={<PublicRoute>{route.component}</PublicRoute>}
-                        />
-                    ))}
-                    {PrivateRoutes.map(route => (
-                        <Route
-                            exact={route.exact}
-                            key={route.path}
-                            path={route.path}
-                            element={<PrivateRoute>{route.component}</PrivateRoute>}
-                        />
-                    ))}
-                </Routes>
-            </Layout>
-        </Suspense>
-    );
+  return (
+    <Suspense className="loader" fallback={<></>}>
+      <Layout style={{ minHeight: '100vh', display: 'flex' }}>
+        <Routes>
+          {PublicRoutes.map(route => (
+            <Route
+              exact={route.exact}
+              key={route.path}
+              path={route.path}
+              element={<PublicRoute>{route.component}</PublicRoute>}
+            />
+          ))}
+          {PrivateRoutes.map(route => (
+            <Route
+              exact={route.exact}
+              key={route.path}
+              path={route.path}
+              element={<PrivateRoute>{route.component}</PrivateRoute>}
+            />
+          ))}
+        </Routes>
+      </Layout>
+    </Suspense>
+  );
 };
 
 export default Routing;
