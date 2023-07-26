3b0016cfcc97086f19d0e61975e6f1abdc2bfa76
NaikDK <dp860359@dal.ca>
2023-07-26 19:55:02 -0300

    Added appointent updates.

--- termprojectteam8/src/Component/Appointment/appointment.js
+++ termprojectteam8/src/Component/Appointment/appointment.js
@@ -1,32 +1,23 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  Calendar,
-  Card,
-  Col,
-
-  Divider,
-  Form,
-
-  Input,
-  Layout,
-  List,
-  Row,
-  Select,
-
-} from 'antd';
-import ArrowRightOutlined, {EnterOutlined} from '@ant-design/icons';
+import { Button, Calendar, Card, Col, Divider, Form, Input, Layout, List, Row, Select } from 'antd';
+import ArrowRightOutlined, { EnterOutlined } from '@ant-design/icons';
 
 import './appointment.css';
 
-
 const { Option } = Select;
-
 const Appointment = () => {
   const [showReceiptNo, setShowReceiptNo] = useState(true);
   const [showLocation, setShowLocation] = useState(false);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showBook, setShowBook] = useState(false);
+  const [slots, setSlots] = useState();
+  const [selectedSlot, setSelectedSlot] = useState(null);
+  const [slot, setSlot] = useState({
+    location: null,
+    date: null,
+    time: null,
+  });
+
   // dayjs.extend(customParseFormat);
   const range = (start, end) => {
     const result = [];
@@ -44,15 +35,18 @@ const Appointment = () => {
   };
 
   const handleLocationChange = value => {
+    setSlot(prevState => ({ ...prevState, location: value }));
     setShowDatePicker(value !== '');
   };
 
   // const disabledDates = localStorage.getItem("appointmentDates");
   const disabledDates = ['2023-07-25', '2023-07-26', '2023-07-27', '2023-07-28', '2023-07-29'];
-  const slots = [
-    { time: '09:00', status: 'Available' },
-    { time: '10:00', status: 'Booked' },
-    { time: '11:00', status: 'Available' },
+  const availableSlots = [
+    '2023-07-28 09:00',
+    '2023-07-27 09:00',
+    '2023-07-27 10:00',
+    '2023-07-29 11:00',
+    '2023-07-29 12:00',
     // Add more slot data as needed
   ];
 
@@ -67,17 +61,31 @@ const Appointment = () => {
     disabledHours: () => range(0, 9).concat(range(17, 24)),
     // disabledMinutes: () => range(),
   });
-  const onChange = (date, dateString) => {
-    console.log(disabledTime());
-    console.log(date, dateString);
+  const onDateChange = date => {
+    // console.log(disabledTime());
+    const filteredSlots = availableSlots.filter(item => item.startsWith(date.format('YYYY-MM-DD')));
+
+    setSlots(filteredSlots.map(item => item.slice(11)));
+
+    setSlot(prevState => ({ ...prevState, date: date.format('YYYY-MM-DD') }));
+    setSelectedSlot(date.format('YYYY-MM-DD'));
+    // console.log(date);
   };
 
   const onSlotSelect = e => {
-    setShowBook(true);
+    // setShowBook(true);
+    setSelectedSlot(prevData => prevData + ' ' + e.value);
+    // setSlot(prevState => ({ ...prevState, time: e.target.innerText }));
     console.log(e);
   };
 
-  const onFinish = () => {};
+  const onFinish = e => {
+    const userData = localStorage.getItem('userData');
+    userData['appointment'] = slot.date + ' ' + slot.time;
+    localStorage.setItem('userData', userData);
+    availableSlots.filter(item => item !== userData.appointment);
+    // localStorage.setItem('availableSlots', availableSlots);
+  };
 
   return (
     <>
@@ -85,12 +93,23 @@ const Appointment = () => {
         <Card title="Please enter the details" className="custom-card">
           <Form {...layout} onFinish={onFinish} className="form-container-dk">
             {showReceiptNo && (
-            <Form.Item className={showLocation?"fadeaway":"fadein"} name="ReceiptNo" label="Receipt No.">
-              <Input addonAfter={<EnterOutlined onClick={handleReceiptNoChange}/>} className="receiptno"  />
-            </Form.Item>
+              <Form.Item
+                className={showLocation ? 'fadeaway' : 'fadein'}
+                name="ReceiptNo"
+                label="Receipt No."
+              >
+                <Input
+                  addonAfter={<EnterOutlined onClick={handleReceiptNoChange} />}
+                  className="receiptno"
+                />
+              </Form.Item>
             )}
             {showLocation && (
-              <Form.Item className={showLocation?"fadein":"fadeaway"} name="location" label="Location">
+              <Form.Item
+                className={showLocation ? 'fadein' : 'fadeaway'}
+                name="location"
+                label="Location"
+              >
                 <Select
                   className="select"
                   placeholder="Select a location"
@@ -99,55 +118,70 @@ const Appointment = () => {
                 >
                   <Option value="Halifax">Halifax</Option>
                   <Option value="Dartmouth">Dartmouth</Option>
-                  <Option value="East Passage">East Passage</Option>
-                  <Option value="Lunenberg">Lunenberg</Option>
+                  <Option value="Sackville">Sackville</Option>
+                  <Option value="Bridgewater">Bridgewater</Option>
+                  <Option value="Kentville">Kentville</Option>
                 </Select>
               </Form.Item>
             )}
 
             {showDatePicker && (
-                <div>
+              <div>
                 {/*<Divider />*/}
-              <Row className={`gutter-row datePicker ${showDatePicker?"fadein":"fadeaway"}`} gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                <Col className="calendar" span={12}>
-                  Select Date:
-                  {/*<Form.Item name="slot">*/}
-                  {/*  <DatePicker*/}
-                  {/*    className="datePicker"*/}
-                  {/*    format="YYYY-MM-DD HH:mm"*/}
-                  {/*    disabledDate={disabledDate}*/}
-                  {/*    // disabledTime={disabledTime}*/}
-                  {/*    onChange={onChange}*/}
-                  {/*    // showTime={{*/}
-                  {/*    //   defaultValue: dayjs('09:00:00', 'HH:mm:ss'),*/}
-                  {/*    //   hideDisabledOptions: true,*/}
-                  {/*    //   format: 'HH:mm',*/}
-                  {/*    //   minuteStep: 30,*/}
-                  {/*    // }}*/}
-                  {/*  />*/}
-                  {/*</Form.Item>*/}
-                  <Calendar disabledDate={disabledDate} fullscreen={false} />
-                </Col>
-                <Col className="slots" span={12}>
-                  Select Slot:
-                  <List
-                    grid={{ gutter: 16, column: 4 }}
-                    dataSource={slots}
-                    renderItem={item => (
-                      <List.Item className="slot">
-                        <Card onClick={onSlotSelect}>{item.time}</Card>
-                      </List.Item>
-                    )}
-                  />
-                </Col>
-              </Row>
-                </div>
+                <Row
+                  className={`gutter-row datePicker ${showDatePicker ? 'fadein' : 'fadeaway'}`}
+                  gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+                >
+                  <Col className="calendar" span={12}>
+                    Select Date:
+                    {/*<Form.Item name="slot">*/}
+                    {/*  <DatePicker*/}
+                    {/*    className="datePicker"*/}
+                    {/*    format="YYYY-MM-DD HH:mm"*/}
+                    {/*    disabledDate={disabledDate}*/}
+                    {/*    // disabledTime={disabledTime}*/}
+                    {/*    onChange={onChange}*/}
+                    {/*    // showTime={{*/}
+                    {/*    //   defaultValue: dayjs('09:00:00', 'HH:mm:ss'),*/}
+                    {/*    //   hideDisabledOptions: true,*/}
+                    {/*    //   format: 'HH:mm',*/}
+                    {/*    //   minuteStep: 30,*/}
+                    {/*    // }}*/}
+                    {/*  />*/}
+                    {/*</Form.Item>*/}
+                    <Calendar
+                      disabledDate={disabledDate}
+                      fullscreen={false}
+                      onChange={onDateChange}
+                    />
+                  </Col>
+                  <Col span={12}>
+                    Select Slot:
+                    <List
+                      className="slots"
+                      grid={{ gutter: 16, column: 4 }}
+                      dataSource={slots}
+                      renderItem={item => (
+                        <List.Item className="slot">
+                          <Button
+                            onClick={() => onSlotSelect(slot)}
+                            type={selectedSlot === item ? 'primary' : 'default'}
+                          >
+                            {item}
+                          </Button>
+                        </List.Item>
+                      )}
+                    />
+                    <span className="selectedSlot">{selectedSlot}</span>
+                  </Col>
+                </Row>
+              </div>
             )}
             {showBook && (
-            <Button className="button" type="primary">
-              <ArrowRightOutlined />
-              Book
-            </Button>
+              <Button className="button" type="primary">
+                <ArrowRightOutlined />
+                Book
+              </Button>
             )}
           </Form>
         </Card>
