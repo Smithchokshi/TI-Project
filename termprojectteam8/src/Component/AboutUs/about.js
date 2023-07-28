beeb3b8526f5143de7d7ce392710226ff7f244b8
Krina Rajeshkumar Mistry <kr981143@dal.ca>
2023-07-27 21:22:39 -0300

    About US Page updated

--- termprojectteam8/src/Component/AboutUs/about.js
+++ termprojectteam8/src/Component/AboutUs/about.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Card, Row, Col, Button, Table, message, Popconfirm  } from 'antd';
-import { CalendarTwoTone, MailTwoTone, EnvironmentTwoTone, RightCircleTwoTone  } from '@ant-design/icons';
+import { Typography, Card, Row, Col, Button, Table, message, Popconfirm } from 'antd';
+import { CalendarTwoTone, MailTwoTone, EnvironmentTwoTone, RightCircleTwoTone } from '@ant-design/icons';
 import '../Dashboard/dashboard.css'
 import { useNavigate } from 'react-router-dom';
 
@@ -119,7 +119,7 @@ const About = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email: email, testLocation: testLocation, testDate: testDate, name: name } )
+            body: JSON.stringify({ email: email, testLocation: testLocation, testDate: testDate, name: name })
         });
     };
     const cancel = (e) => {
@@ -188,48 +188,48 @@ const About = () => {
                 <Title >Access Nova Scotia: About Us</Title>
                 <Row style={{ marginTop: '15px' }} gutter={[16, 16]}>
                     <Col span={16}>
-                            <div className="scroll-height">
-                                <br />
-                                <Title style={{ fontSize: '20px' }}>What we do</Title>
-                                <div className="ullist" style={{ fontSize: '17px' }}>
-                                    The Department of Community Services helps people live more independent and healthier lives by providing a range of social services. We work together with organizations across the province to administer social programs, including employment support and skills training, income assistance, affordable housing and youth and family supports.
-                                </div>
-                                <br/>
-                                <div className="ullist" style={{ fontSize: '17px' }}>
-                                    We’re also responsible for child protection services, the foster care system and disability support programs.
-                                </div>
-                                <br/>
-                                <Title style={{ fontSize: '20px' }}>Responsibilities </Title>
-                                <div style={{ fontSize: '17px' }}> We're responsible for:</div>
-                                <ul className="ullist" style={{ fontSize: '17px' }}>
-                                    <li>helping make sure that children receive care essential for their wellbeing</li>
-                                    <li>providing employment support and income assistance</li>
-                                    <li>working with other departments and community housing organizations to provide supports for people who are experiencing homelessness</li>
-                                    <li>providing programs to help youth at risk</li>
-                                    <li>managing the Nova Scotia Child Benefit Program, which helps low-income families with the cost of raising children</li>
-                                    <li>helping create inclusive opportunities for people with intellectual disabilities, long-term mental illness and physical disabilities to live more independent and self-reliant lives</li>
-                                    <li>working with the Nova Scotia Advisory Council on the Status of Women to make sure that issues affecting women (like equality and fairness) are part of government’s planning</li>
-                                </ul>
-                                <br />
-                                <Title style={{ fontSize: '20px' }}>Who we are</Title>
-                                <div className="ullist" style={{ fontSize: '17px' }}>
-                                    The Department of Community Services has about 1,500 staff in 30 offices across the province.
-                                </div>
-                                <br />
-                                <Title style={{ fontSize: '20px' }}>Priorities</Title>
-                                <div className="ullist" style={{ fontSize: '17px' }}>
-                                    Our priorities are:
-                                </div>
-                                <ul className="ullist" style={{ fontSize: '17px' }}>
-                                    <li>helping individuals living with disabilities become more independent through increased community-based programming</li>
-                                    <li>providing employment support, skills training and funding for post-secondary education to help people get the skills and experience they need for work</li>
-                                    <li>improving the way the department supports people in need, including people experiencing intergenerational poverty and in racialized communities</li>
-                                    <li>working with partners and communities to find ways to reduce poverty and address homelessness throughout the province</li>
-                                    <li>working with partners to support recommendations and plans from the Restorative Inquiry on the Nova Scotia Home for Colored Children</li>
-                                    <li>continuing to expand placement options for children and youth in the child welfare system</li>
-                                    <li>completing a review of the foster care program to inform changes that will help recruit and retain the next generation of foster families</li>
-                                </ul>
+                        <div className="scroll-height">
+                            <br />
+                            <Title style={{ fontSize: '20px' }}>What we do</Title>
+                            <div className="ullist" style={{ fontSize: '17px' }}>
+                                The Department of Community Services helps people live more independent and healthier lives by providing a range of social services. We work together with organizations across the province to administer social programs, including employment support and skills training, income assistance, affordable housing and youth and family supports.
                             </div>
+                            <br />
+                            <div className="ullist" style={{ fontSize: '17px' }}>
+                                We’re also responsible for child protection services, the foster care system and disability support programs.
+                            </div>
+                            <br />
+                            <Title style={{ fontSize: '20px' }}>Responsibilities </Title>
+                            <div style={{ fontSize: '17px' }}> We're responsible for:</div>
+                            <ul className="ullist" style={{ fontSize: '17px' }}>
+                                <li>helping make sure that children receive care essential for their wellbeing</li>
+                                <li>providing employment support and income assistance</li>
+                                <li>working with other departments and community housing organizations to provide supports for people who are experiencing homelessness</li>
+                                <li>providing programs to help youth at risk</li>
+                                <li>managing the Nova Scotia Child Benefit Program, which helps low-income families with the cost of raising children</li>
+                                <li>helping create inclusive opportunities for people with intellectual disabilities, long-term mental illness and physical disabilities to live more independent and self-reliant lives</li>
+                                <li>working with the Nova Scotia Advisory Council on the Status of Women to make sure that issues affecting women (like equality and fairness) are part of government’s planning</li>
+                            </ul>
+                            <br />
+                            <Title style={{ fontSize: '20px' }}>Who we are</Title>
+                            <div className="ullist" style={{ fontSize: '17px' }}>
+                                The Department of Community Services has about 1,500 staff in 30 offices across the province.
+                            </div>
+                            <br />
+                            <Title style={{ fontSize: '20px' }}>Priorities</Title>
+                            <div className="ullist" style={{ fontSize: '17px' }}>
+                                Our priorities are:
+                            </div>
+                            <ul className="ullist" style={{ fontSize: '17px' }}>
+                                <li>helping individuals living with disabilities become more independent through increased community-based programming</li>
+                                <li>providing employment support, skills training and funding for post-secondary education to help people get the skills and experience they need for work</li>
+                                <li>improving the way the department supports people in need, including people experiencing intergenerational poverty and in racialized communities</li>
+                                <li>working with partners and communities to find ways to reduce poverty and address homelessness throughout the province</li>
+                                <li>working with partners to support recommendations and plans from the Restorative Inquiry on the Nova Scotia Home for Colored Children</li>
+                                <li>continuing to expand placement options for children and youth in the child welfare system</li>
+                                <li>completing a review of the foster care program to inform changes that will help recruit and retain the next generation of foster families</li>
+                            </ul>
+                        </div>
                     </Col>
                 </Row>
             </Card>
